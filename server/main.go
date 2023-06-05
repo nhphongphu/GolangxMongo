9c7c05d8aeee9fece9f33a2bf3c74a1b1d8687dc
phongphu <phu.nhp@vn-cubesystem.com>
2023-06-05 12:15:17 +0700

    first commit

---
+++ server/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"server/configs"
+	"server/routes"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	//run database
+	configs.ConnectDB()
+
+	//routes
+	routes.UserRoute(app)
+
+	app.Listen(":6000")
+}
